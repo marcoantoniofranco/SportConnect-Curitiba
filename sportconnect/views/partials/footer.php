41ab8d35b96aefaee73753e670f8f833455006c8
mfrickss <mfricks777@gmail.com>
2025-06-09 17:23:19 -0300

    add pasta partials com footer && header

---
+++ sportconnect/views/partials/footer.php
@@ -0,0 +1,41 @@
+<?php if (!defined('FONT_AWESOME_LOADED')): ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <?php define('FONT_AWESOME_LOADED', true); ?>
+<?php endif; ?>
+
+
+<footer class="site-footer">
+    <div class="footer-content">
+        <div class="footer-section">
+            <h4>SportConnect Curitiba</h4>
+            <p>Conectando atletas e entusiastas do esporte em Curitiba</p>
+            <div class="social-links">
+                <a href="#" class="social-link"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="social-link"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="social-link"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+        
+        <div class="footer-section">
+            <h4>Links Rápidos</h4>
+            <ul>
+                <li><a href="/SportConnect-Curitiba/sportconnect/home/index">Início</a></li>
+                <li><a href="/SportConnect-Curitiba/sportconnect/home/sobre">Sobre</a></li>
+                <li><a href="/SportConnect-Curitiba/sportconnect/home/contato">Contato</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-section">
+            <h4>Esportes</h4>
+            <ul>
+                <li><a href="#">Futebol</a></li>
+                <li><a href="#">Vôlei</a></li>
+                <li><a href="#">Basquete</a></li>
+                <li><a href="#">Corrida</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; 2025 SportConnect Curitiba. Todos os direitos reservados.</p>
+    </div>
+</footer>
---
+++ sportconnect/views/partials/header.php
@@ -0,0 +1,20 @@
+<header class="site-header">
+    <div class="header-container">
+        <div class="logo">
+            <a href="/SportConnect-Curitiba/sportconnect/home/index">
+                <span class="logo-text">SportConnect</span>
+                <span class="logo-city">Curitiba</span>
+            </a>
+        </div>
+        
+        <nav class="main-nav">
+            <ul class="nav-list">
+                <li><a href="/SportConnect-Curitiba/sportconnect/home/index">Início</a></li>
+                <li><a href="/SportConnect-Curitiba/sportconnect/home/sobre">Sobre</a></li>
+                <li><a href="/SportConnect-Curitiba/sportconnect/home/contato">Contato</a></li>
+                <li class="nav-button"><a href="/SportConnect-Curitiba/sportconnect/login" class="btn-login">Entrar</a></li>
+                <li class="nav-button"><a href="/SportConnect-Curitiba/sportconnect/register" class="btn-register">Cadastrar</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
