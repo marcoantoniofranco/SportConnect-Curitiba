7f69f8d855a0f5870f5e44cf0c9a7b3995ea049d
mfrickss <mfricks777@gmail.com>
2025-06-09 17:21:55 -0300

    index.php root

---
+++ sportconnect/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'controllers/HomeController.php';
+require_once 'controllers/ProfileController.php';
+
+$url = isset($_GET['url']) ? $_GET['url'] : 'home/index';
+$url = rtrim($url, '/');
+$url = explode('/', $url);
+
+$controllerName = isset($url[0]) ? $url[0] : 'home';
+$methodName = isset($url[1]) ? $url[1] : 'index';
+
+$controllerName = ucfirst($controllerName) . 'Controller';
+
+if (class_exists($controllerName)) {
+    $controller = new $controllerName();
+    if (method_exists($controller, $methodName)) {
+        $controller->$methodName();
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        echo "Página não encontrada";
+    }
+} else {
+    header("HTTP/1.0 404 Not Found");
+    echo "Página não encontrada";
+}
+?>
