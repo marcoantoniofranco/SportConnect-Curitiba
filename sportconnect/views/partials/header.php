0c3463bf0558be83aad6fd0e524e9db02d2abba4
mfrickss <mfricks777@gmail.com>
2025-06-11 13:04:08 -0300

    Ajusta links e padroniza rotas no header

--- sportconnect/views/partials/header.php
+++ sportconnect/views/partials/header.php
@@ -12,8 +12,8 @@
                 <li><a href="/SportConnect-Curitiba/sportconnect/home/index">Início</a></li>
                 <li><a href="/SportConnect-Curitiba/sportconnect/home/sobre">Sobre</a></li>
                 <li><a href="/SportConnect-Curitiba/sportconnect/home/contato">Contato</a></li>
-                <li class="nav-button"><a href="/SportConnect-Curitiba/sportconnect/login" class="btn-login">Entrar</a></li>
-                <li class="nav-button"><a href="/SportConnect-Curitiba/sportconnect/register" class="btn-register">Cadastrar</a></li>
+                <li class="nav-button"><a href="/SportConnect-Curitiba/sportconnect/auth/login" class="btn-login">Entrar</a></li>
+                <li class="nav-button"><a href="/SportConnect-Curitiba/sportconnect/auth/register" class="btn-register">Cadastrar</a></li>
             </ul>
         </nav>
     </div>
